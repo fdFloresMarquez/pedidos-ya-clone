6e4abdd3acb4cef6fc6732d28d2906003e54a467
Facundo <floresfacundo68@gmail.com>
2022-09-30 21:06:14 -0300

    add data in js

---
+++ src/data/categories.js
@@ -0,0 +1,41 @@
+import location from '/assets/circularIcons/location.svg';
+import food from '/assets/circularIcons/food.svg';
+import shield from '/assets/circularIcons/shield.svg';
+
+export const ciudades = {
+    heading: 'Ciudades',
+    icon: location,
+    sections: [
+        { title: 'La Plata', anchor:'/restaurantes/la-plata' },
+        { title: 'San Isidro', anchor:'/restaurantes/san-isidro'},
+        { title: 'Córdoba', anchor: '/restaurantes/cordoba'},
+        { title: 'Buenos Aires', anchor: '/restaurantes/buenos-aires'},
+        { title: 'Vicente López', anchor: '/restaurantes/vicente-lopez'},
+    ]
+};
+
+export const barrios = {
+    heading: 'Barrio',
+    icon: shield,
+    sections: [
+        { title: 'Palermo', anchor:'/restaurantes/buenos-aires/palermo' },
+        { title: 'Caballito', anchor:'/restaurantes/buenos-aires/caballito'},
+        { title: 'Belgrano', anchor: '/restaurantes/buenos-aires/belgrano'},
+        { title: 'Recoleta', anchor: '/restaurantes/buenos-aires/recoleta'},
+        { title: 'Microcentro', anchor: '/restaurantes/buenos-aires/microcentro'},
+        { title: 'Nueva Córdoba', anchor: '/restaurantes/buenos-aires/nueva-cordoba'},
+    ]
+};
+
+export const comidas = {
+    heading: 'Comidas',
+    icon: food,
+    sections: [
+        { title: 'Sushi', anchor:'/comidas/sushi' },
+        { title: 'Picadas', anchor:'/comidas/picadas'},
+        { title: 'Empanadas', anchor: '/comidas/empanadas'},
+        { title: 'Desayunos', anchor: '/comidas/desayunos'},
+        { title: 'Helados', anchor: '/comidas/helados'},
+        { title: 'Pizzas', anchor: '/comidas/pizzas'},
+    ]
+}
---
+++ src/getImages.js
@@ -0,0 +1,26 @@
+import burgerking from '/assets/storeImages/burger-king.png';
+import almacen_de_pizzas from '/assets/storeImages/almacen-de-pizzas.png';
+import carrefour from '/assets/storeImages/carrefour.png';
+import el_club_de_la_milanesa from '/assets/storeImages/el_club_de_la_milanesa.png';
+import el_noble from '/assets/storeImages/el_noble.png';
+import fabric from '/assets/storeImages/fabric.png';
+import la_farola from '/assets/storeImages/la-farola.png';
+import morita from '/assets/storeImages/morita.png';
+import pain_quotidien from '/assets/storeImages/pain_quotidien.png';
+import subway from '/assets/storeImages/subway.png';
+import sushi_pop from '/assets/storeImages/sushi-pop.png';
+
+
+export const getImages = [
+    burgerking,
+    almacen_de_pizzas,
+    carrefour,
+    el_club_de_la_milanesa,
+    el_noble,
+    fabric,
+    la_farola,
+    morita,
+    pain_quotidien,
+    subway,
+    sushi_pop,
+]
