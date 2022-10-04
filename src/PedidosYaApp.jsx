5ee791357e2fa6524244141c50983d5571b70630
Facundo <floresfacundo68@gmail.com>
2022-10-04 15:42:16 -0300

    add sweetalert

--- src/PedidosYaApp.jsx
+++ src/PedidosYaApp.jsx
@@ -1,14 +1,21 @@
 
 import { Stack } from '@chakra-ui/react';
+import Swal from 'sweetalert2';
 
 import { BodySection, Footer, NavBar, SearchSection } from './components';
 
 
 export const PedidosYaApp = () => {
 
+  Swal.fire(
+    'Atencion!',
+    'Este sitio web es un <b>clon</b> utilizado como demostración de front-end, no debe ser tomado como la página real de pedidos-ya',
+    'info'
+  )
+
   return (
     <Stack spacing='0'>
-
+      
       <NavBar />
 
       <SearchSection />
